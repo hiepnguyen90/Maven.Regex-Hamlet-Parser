6ccdbc82afbef3cffb263ef2bf14f3ccb086a10a
hiepnguyen90 <johnhiepnguyen1@gmail.com>
2023-04-10 18:19:55 -0400

    done

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -36,4 +38,67 @@ public class HamletParser {
         return hamletData;
     }
 
+
+    public void changeHamletToLeon() {
+        Pattern pattern = Pattern.compile("Hamlet");
+        Matcher matcher = pattern.matcher(hamletData);
+        String replacementText = "Leon";
+        StringBuffer sb = new StringBuffer();
+
+        while(matcher.find()){
+           // matchedText = matcher.group();
+            matcher.appendReplacement(sb, replacementText);
+        }
+        hamletData = matcher.appendTail(sb).toString();
+
+        pattern = Pattern.compile("HAMLET");
+        matcher = pattern.matcher(hamletData);
+        replacementText = "LEON";
+        sb = new StringBuffer();
+
+        while(matcher.find()){
+       // matchedText = matcher.group();
+        matcher.appendReplacement(sb, replacementText);
+        }
+
+        hamletData = matcher.appendTail(sb).toString();
+}
+
+
+    public void changeHoratioToTariq() {
+        Pattern pattern = Pattern.compile("Horatio");
+        Matcher matcher = pattern.matcher(hamletData);
+        String replacementText = "Tariq";
+        StringBuffer sb = new StringBuffer();
+
+        while(matcher.find()){
+            // matchedText = matcher.group();
+            matcher.appendReplacement(sb, replacementText);
+        }
+        hamletData = matcher.appendTail(sb).toString();
+
+        pattern = Pattern.compile("HORATIO");
+        matcher = pattern.matcher(hamletData);
+        replacementText = "TARIQ";
+        sb = new StringBuffer();
+
+        while(matcher.find()){
+            // matchedText = matcher.group();
+            matcher.appendReplacement(sb, replacementText);
+        }
+
+        hamletData = matcher.appendTail(sb).toString();
+    }
+
+    public int findHoratio() {
+        Pattern pattern = Pattern.compile("Horatio", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletData);
+        return (int) matcher.results().count();
+    }
+
+    public int findHamlet() {
+        Pattern pattern = Pattern.compile("Hamlet", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletData);
+        return (int) matcher.results().count();
+    }
 }
